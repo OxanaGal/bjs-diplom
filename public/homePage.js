2107d447f3af53c60b6716c6c4de326758ea01cc
Oxana Galyamova <mrs.oxas@gmail.com>
2022-01-15 00:28:58 +0500

    info

--- public/homePage.js
+++ public/homePage.js
@@ -5,13 +5,17 @@ const userLogOut = new LogoutButton;
 
 userLogOut.action = (data) => {
     return ApiConnector.logout(response =>{
-        if(responce.success === true){
+        if(response.success === true){
             location.reload();
         }
     });
 }
 // получение информации о пользователе
-ApiConnector.current
+ApiConnector.current(response =>{
+    if(response.success === true){
+        ProfileWidget.showProfile(response.data);
+    }
+})
 
 // получение текущиз курсов валют
 // операции с деньгами
